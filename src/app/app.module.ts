089f50e124b399026431b5482841ed9cdbcce1e2
Pawel Wnuk <vvnuczek@VVNUCZEK-MacBook.local>
2020-02-23 13:37:13 +0100

    Angular material imports change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTableModule } from '@angular/material/table';
-import {  MatInputModule,
-          MatPaginatorModule,
-          MatSortModule,
-          MatProgressSpinnerModule,
-          MatButtonModule,
-          MatRadioModule,
-          MatOptionModule,
-          MatSelectModule,
-          MatDialogModule,
-          MatDialogRef,
-          MatCardModule,
-          MatSidenavModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
